c252e4d4e83bde544ec1b15a2d8862011f588080
Ivan Daniel <84136621+doctoxer@users.noreply.github.com>
2025-01-06 17:39:53 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/doctoxer/Lucy.git /Lucy
+  git clone https://github.com/doctoxer/AutoFilterBot.git /AutoFilterBot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Lucy
+  git clone $UPSTREAM_REPO /AutoFilterBot
 fi
-cd /Lucy
+cd /AutoFilterBot
 pip3 install -U -r requirements.txt
 echo "sᴛᴀʀᴛɪɴɢ ʟᴜᴄʏ ʙᴏᴛ...."
 python3 bot.py
