1a50f512e095a989a9a4aa46e253a9d8ba0770d9
Ivan Daniel <84136621+doctoxer@users.noreply.github.com>
2025-01-08 01:24:47 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -35,7 +35,7 @@ async def answer(bot, query):
     if AUTH_CHANNEL and not await is_req_subscribed(bot, query):
         await query.answer(results=[],
                            cache_time=0,
-                           switch_pm_text='You have to subscribe my channel to use the bot',
+                           switch_pm_text='You have to subscribe my channel to use the bot ¯\_(ツ)_/¯',
                            switch_pm_parameter="subscribe")
         return
 
@@ -107,7 +107,7 @@ async def answer(bot, query):
 def get_reply_markup(query):
     buttons = [
         [
-            InlineKeyboardButton('Search again', switch_inline_query_current_chat=query)
+            InlineKeyboardButton('search again ⌕', switch_inline_query_current_chat=query)
         ]
         ]
     return InlineKeyboardMarkup(buttons)
